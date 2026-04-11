fe9a6f31d3998b29a99894541a0239b3027ee8ce
Ngangoula Jaures <jaures.ngangoula@epitech.eu>
2026-04-11 03:24:35 +0000

    CHORE(routes): création de fichiers routes distinct

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,15 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+require __DIR__.'/auth.php';
+require __DIR__.'/admin.php';
+require __DIR__.'/comments.php';
+require __DIR__.'/profile.php';
+
 Route::get('/', function () {
     return Inertia::render('Welcome');
 });
 
-Route::get('/test', function () {
-    return Inertia::render('test');
-});
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// Route::get('/test', function () {
+//     return Inertia::render('test');
 // });
